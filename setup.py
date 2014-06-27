ac77deb08bcc932e921142f8573d0827787bfe39
Cary M. Robbins <carymrobbins@gmail.com>
2014-06-26 20:41:32 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
+from setuptools import setup, find_packages
 
 setup(
     name="tryhaskell",
     version="0.1.0",
     author="Cary Robbins",
     author_email="carymrobbins@gmail.com",
-    url="{{homepage}}",
-    packages=["tryhaskell"],
+    url="https://github.com/carymrobbins/py-tryhaskell",
+    packages=find_packages(),
     description="Python client for tryhaskell.org",
-    long_description=open('README.rst').read(),
     license=open('LICENSE').read(),
     install_requires=[
-        'requests==2.3.0',
+        'requests',
     ],
     classifiers=[
     ]
